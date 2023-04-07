6a4e70fdf07f040d7bdd9bf65381394d038d1753
AlyonaEfros <AlyonaEfros@yandex.ru>
2023-04-07 19:34:09 +0300

    +Allure2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,18 +19,13 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.holdBrowserOpen = false;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
         Configuration.browserCapabilities = capabilities;
     }
 
