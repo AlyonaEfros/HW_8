43cf63a49b673b3ded789abb7d223ff57b3ba2b3
AlyonaEfros <AlyonaEfros@yandex.ru>
2023-04-11 15:00:26 +0300

    add parameters

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,9 +17,11 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoidAddress");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
